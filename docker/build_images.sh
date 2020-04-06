4d07ef07ddee835c5520132dc3e994b63bbd39f6
Zhipeng Jia <zjia@cs.utexas.edu>
2020-04-06 16:58:09 -0500

    tweak golang env

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -14,3 +14,11 @@ TAG=git-$GIT_HASH-$DATE
 ( cd $ROOT_DIR &&
   docker build -t zjia/faas-worker-golang:$TAG -f ./docker/Dockerfile.worker-golang \
                --build-arg WATCHDOG_TAG=$TAG . )
+( cd $ROOT_DIR &&
+  docker build -t zjia/faas-golang-env:$TAG -f ./docker/Dockerfile.golang-env . )
+
+docker tag zjia/faas-gateway:$TAG zjia/faas-gateway:latest
+docker tag zjia/faas-watchdog-bionic:$TAG zjia/faas-watchdog-bionic:latest
+docker tag zjia/faas-watchdog-alpine:$TAG zjia/faas-watchdog-alpine:latest
+docker tag zjia/faas-worker-golang:$TAG zjia/faas-worker-golang:latest
+docker tag zjia/faas-golang-env:$TAG zjia/faas-golang-env:latest
